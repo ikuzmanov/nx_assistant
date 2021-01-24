5d00e2e735129dd33aab63946702898d458944a9
Ivaylo <ivaylo.kuzmanov@gmail.com>
2021-01-24 22:22:55 +0200

    first commit for NX Assistant

---
+++ NX_Assistant/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace NX_Assistant
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Button button1;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip3 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip4 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip5 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip6 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip7 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip8 = new System.Windows.Forms.ToolTip(this.components);
+            button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.AutoEllipsis = true;
+            button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            button1.Image = global::NX_Assistant.Properties.Resources.pin_icon1;
+            button1.Location = new System.Drawing.Point(93, 1);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(23, 22);
+            button1.TabIndex = 2;
+            this.toolTip8.SetToolTip(button1, "Toggle always on top");
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 19);
+            this.textBox1.MaxLength = 6;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(46, 67);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(37, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "NX";
+            this.toolTip3.SetToolTip(this.button2, "Nexus project");
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(46, 125);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(37, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "OD";
+            this.toolTip7.SetToolTip(this.button3, "OneDrive project folder");
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(6, 96);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(37, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "MQ";
+            this.toolTip4.SetToolTip(this.button4, "memoQ server project");
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(6, 67);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(37, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "FS";
+            this.toolTip2.SetToolTip(this.button5, "File server project directory");
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(46, 96);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(37, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "OB";
+            this.toolTip5.SetToolTip(this.button6, "Obfuscator");
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(6, 125);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(37, 23);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "DL";
+            this.toolTip6.SetToolTip(this.button7, "Batch delivery function");
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Button7_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.groupBox1.Controls.Add(this.button9);
+            this.groupBox1.Controls.Add(this.button8);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button7);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.groupBox1.Location = new System.Drawing.Point(6, 1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(98, 179);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Project ID";
+            this.groupBox1.Enter += new System.EventHandler(this.GroupBox1_Enter);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(6, 154);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(37, 23);
+            this.button9.TabIndex = 11;
+            this.button9.Text = "ORG";
+            this.toolTip8.SetToolTip(this.button9, "Takes all the files in _src and organises your project folder structure");
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.Button9_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button8.Image = global::NX_Assistant.Properties.Resources.multy_user1;
+            this.button8.Location = new System.Drawing.Point(6, 41);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(37, 20);
+            this.button8.TabIndex = 10;
+            this.toolTip1.SetToolTip(this.button8, "Your O365 username ");
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.Button8_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(46, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(35, 20);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.Popup += new System.Windows.Forms.PopupEventHandler(this.ToolTip1_Popup);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(116, 186);
+            this.Controls.Add(button1);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "NX Assistant";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolTip toolTip2;
+        private System.Windows.Forms.ToolTip toolTip3;
+        private System.Windows.Forms.ToolTip toolTip4;
+        private System.Windows.Forms.ToolTip toolTip5;
+        private System.Windows.Forms.ToolTip toolTip6;
+        private System.Windows.Forms.ToolTip toolTip7;
+        private System.Windows.Forms.ToolTip toolTip8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ NX_Assistant/Form1.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NX_Assistant.Properties;
+
+namespace NX_Assistant
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            // text field for the project number / custom validation for inputing 6 digits only
+            if (System.Text.RegularExpressions.Regex.IsMatch(textBox1.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Please enter numbers only (6 digits)","Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1);
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            // toggles/ untoggles the app on top of all 
+            this.TopMost ^= true;
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            // WIP - expected solution - to open the user's personal project folder in OneDrive
+            // string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+            // System.Diagnostics.Debug.WriteLine(userName);
+            var project_id = textBox1.Text;
+            var O365user = textBox2.Text;
+            if (String.IsNullOrEmpty(textBox1.Text) || textBox1.Text.Length < 6)
+            {
+                MessageBox.Show("Please input the correct project number (6 digits)", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+                System.Diagnostics.Process.Start("https://stptrans-my.sharepoint.com/personal/"+O365user+"_stptrans_com/_layouts/15/onedrive.aspx?id=%2Fpersonal%2F"+O365user+"_stptrans_com%2FDocuments%2F" + project_id);
+        }
+
+        private void Button6_Click(object sender, EventArgs e)
+        {
+            // runs the STP obfuscator
+            System.Diagnostics.Process.Start("iexplore", "http://nx/Wiki/Page/Apps/XliffMtObfuscator/WindowsApplication.application");
+        }
+
+        private void Button7_Click(object sender, EventArgs e)
+        {
+            // takes the project number as argument, stores it into a variable, opens the project folder on the FS, the MQ project in NX and the obfuscator
+
+            var project_id = textBox1.Text;
+            if (String.IsNullOrEmpty(textBox1.Text) || textBox1.Text.Length < 6)
+            {
+                MessageBox.Show("Please input the correct project number (6 digits)", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                System.Diagnostics.Process.Start("iexplore", "http://nx/Wiki/Page/Apps/XliffMtObfuscator/WindowsApplication.application");
+                System.Diagnostics.Process.Start("http://nx/MemoQ/ServerProject/Select.aspx?sll=&tll=&qn=" + project_id + "&qc=&qd=&qp=&qs=&qx=&sd1=&sd2=");
+                string string1 = project_id.Substring(0, 2);
+                string string2 = project_id.Substring(2, 2);
+                string string3 = project_id.Substring(4, 2);
+                var output = @"\\fs\P\STP\00\" + string1 + @"\" + string2 + @"\" + string3 + @"\";
+                try
+                {
+                    System.Diagnostics.Process.Start(output);
+                }
+                catch (System.ComponentModel.Win32Exception)
+                {
+                    MessageBox.Show("The network path was not found, please make sure you've input the correct project number or created the folder structure.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+         }
+
+            private void GroupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            // opens the project in NX
+            var project_id = textBox1.Text;
+            if (String.IsNullOrEmpty(textBox1.Text) || textBox1.Text.Length < 6)
+            {
+                MessageBox.Show("Please input the correct project number (6 digits)", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+                System.Diagnostics.Process.Start("http://nx/Project/Structure/Detail2.aspx?px=1&p="+project_id);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // sets the default username for the OD button to work
+           textBox2.Text = Settings.Default.O365;
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            // opens the project folder on the file server
+            var project_id = textBox1.Text;
+            if (String.IsNullOrEmpty(textBox1.Text) || textBox1.Text.Length < 6)
+            {
+                MessageBox.Show("Please input the correct project number (6 digits)", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                string string1 = project_id.Substring(0, 2);
+                string string2 = project_id.Substring(2, 2);
+                string string3 = project_id.Substring(4, 2);
+                var output = @"\\fs\P\STP\00\" + string1 + @"\" + string2 + @"\" + string3 + @"\";
+                try
+                {
+                    System.Diagnostics.Process.Start(output);
+                }
+                catch (System.ComponentModel.Win32Exception)
+                {
+                    MessageBox.Show("The network path was not found, please make sure you've input the correct project number or created the folder structure.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+               
+            }
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            // opens the MQ server project in NX
+            var project_id = textBox1.Text;
+            if (String.IsNullOrEmpty(textBox1.Text) || textBox1.Text.Length < 6)
+            {
+                MessageBox.Show("Please input the correct project number (6 digits)", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+                System.Diagnostics.Process.Start("http://nx/MemoQ/ServerProject/Select.aspx?sll=&tll=&qn="+project_id+"&qc=&qd=&qp=&qs=&qx=&sd1=&sd2=");
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button8_Click(object sender, EventArgs e)
+        {
+            // Button to save the defaut username in the app settings - works with OD button
+            var O365text = textBox2.Text;
+            Settings.Default["O365"] = textBox2.Text;
+            Settings.Default.Save();
+        }
+
+        private void ToolTip1_Popup(object sender, PopupEventArgs e)
+        {
+
+        }
+
+        private void Button9_Click(object sender, EventArgs e)
+{
+            var project_id = textBox1.Text;
+            string string1 = project_id.Substring(0, 2);
+            string string2 = project_id.Substring(2, 2);
+            string string3 = project_id.Substring(4, 2);
+            var sourcePath = @"\\fs\P\STP\00\" + string1 + @"\" + string2 + @"\" + string3 + @"\_src";
+            // Bilingual files organisation
+            var targetPath_mq = @"\\fs\P\STP\00\" + string1 + @"\" + string2 + @"\" + string3 + @"\_mq";
+            var extensions_mq = new[] { ".sdlxliff", ".mqxliff" };
+            {
+                try
+                {
+                    var files_mq = (from file in System.IO.Directory.EnumerateFiles(sourcePath, "*", SearchOption.AllDirectories)
+                                    where extensions_mq.Contains(System.IO.Path.GetExtension(file), StringComparer.InvariantCultureIgnoreCase)
+                                    select new
+                                    {
+                                        Source = file,
+                                        Destination = file.Replace(sourcePath, targetPath_mq)
+                                    });
+
+                    foreach (var file in files_mq)
+                    {
+                        Directory.CreateDirectory(Path.GetDirectoryName(file.Destination));
+                        File.Copy(file.Source, file.Destination);
+                    }
+                }
+                catch (System.ComponentModel.Win32Exception)
+                {
+                    MessageBox.Show("The network path was not found, please make sure you've input the correct project number or created the folder structure.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (System.IO.IOException)
+                {
+                    MessageBox.Show("Bilingual files are already organised", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            // TMs organisation
+            var targetPath_tm = @"\\fs\P\STP\00\" + string1 + @"\" + string2 + @"\" + string3 + @"\_tm"; ;
+            var extensions_tm = new[] { ".sdltm", ".tmx" };
+
+            {
+                try
+                {
+                    var files_tm = (from file in System.IO.Directory.EnumerateFiles(sourcePath, "*", SearchOption.AllDirectories)
+                                    where extensions_tm.Contains(System.IO.Path.GetExtension(file), StringComparer.InvariantCultureIgnoreCase)
+                                    select new
+                                    {
+                                        Source = file,
+                                        Destination = file.Replace(sourcePath, targetPath_tm)
+                                    });
+                    foreach (var file in files_tm)
+                    {
+                        Directory.CreateDirectory(Path.GetDirectoryName(file.Destination));
+                        File.Copy(file.Source, file.Destination);
+                    }
+                }
+                catch (System.ComponentModel.Win32Exception)
+                {
+                    MessageBox.Show("The network path was not found, please make sure you've input the correct project number or created the folder structure.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (System.IO.IOException)
+                {
+                    MessageBox.Show("TMs are already organised", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+            // TBs organisation
+            var targetPath_tb = @"\\fs\P\STP\00\" + string1 + @"\" + string2 + @"\" + string3 + @"\_term";
+            var extensions_tb = new[] { ".sdltb", ".tbx", ".txt" };
+           
+            {
+                try
+                {
+                    var files_tb = (from file in System.IO.Directory.EnumerateFiles(sourcePath, "*", SearchOption.AllDirectories)
+                                    where extensions_tb.Contains(System.IO.Path.GetExtension(file), StringComparer.InvariantCultureIgnoreCase)
+                                    select new
+                                    {
+                                        Source = file,
+                                        Destination = file.Replace(sourcePath, targetPath_tb)
+                                    });
+                    foreach (var file in files_tb)
+                    {
+                        Directory.CreateDirectory(Path.GetDirectoryName(file.Destination));
+                        File.Copy(file.Source, file.Destination);
+                    }
+                }
+                catch (System.ComponentModel.Win32Exception)
+                {
+                    MessageBox.Show("The network path was not found, please make sure you've input the correct project number or created the folder structure.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (System.IO.IOException)
+                {
+                    MessageBox.Show("TBs are already organised", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            var targetPath_pm = @"\\fs\P\STP\00\" + string1 + @"\" + string2 + @"\" + string3 + @"\_pm";
+            var extensions_pm = new[] { ".msg", ".xls", ".xlsx", ".csv" };
+            {
+                try
+                {
+                    var files_pm = (from file in System.IO.Directory.EnumerateFiles(sourcePath, "*", SearchOption.AllDirectories)
+                                    where extensions_pm.Contains(System.IO.Path.GetExtension(file), StringComparer.InvariantCultureIgnoreCase)
+                                    select new
+                                    {
+                                        Source = file,
+                                        Destination = System.IO.Path.Combine(targetPath_pm, System.IO.Path.GetFileName(file))
+                                    });
+                    foreach (var file in files_pm)
+                        File.Move(file.Source, file.Destination);
+                }
+                catch (System.ComponentModel.Win32Exception)
+                {
+                    MessageBox.Show("The network path was not found, please make sure you've input the correct project number or created the folder structure.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (System.IO.IOException)
+                {
+                    MessageBox.Show("PM files are already organised", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            var targetPath_ref = @"\\fs\P\STP\00\" + string1 + @"\" + string2 + @"\" + string3 + @"\_ref";
+            var extensions_ref = new[] { ".doc", ".docx", ".pdf" };
+
+            {
+                try
+                {
+                    var files_ref = (from file in System.IO.Directory.EnumerateFiles(sourcePath, "*", SearchOption.AllDirectories)
+                                     where extensions_ref.Contains(System.IO.Path.GetExtension(file), StringComparer.InvariantCultureIgnoreCase)
+                                     select new
+                                     {
+                                         Source = file,
+                                         Destination = System.IO.Path.Combine(targetPath_ref, System.IO.Path.GetFileName(file))
+                                     });
+                    foreach (var file in files_ref)
+                        File.Move(file.Source, file.Destination);
+                }
+                catch (System.ComponentModel.Win32Exception)
+                {
+                    MessageBox.Show("The network path was not found, please make sure you've input the correct project number or created the folder structure.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (System.IO.IOException)
+                {
+                    MessageBox.Show("Refeence files are already organised", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                MessageBox.Show("Done", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
